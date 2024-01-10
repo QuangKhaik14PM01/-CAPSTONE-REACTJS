8bf679573c7a64412182cb12b98eb0b24faa441e
QuangKhaik14PM01 <tranquangkhai2992002@gmai.com>
2024-01-10 20:38:45 +0700

    đẩy lại

--- src/App.js
+++ src/App.js
@@ -41,13 +41,13 @@ function App() {
         <UserTemplate path="/login" exact Component={Login} />
         <UserTemplate path="/register" exact Component={Register} />
         
-        <AdminTemplate path="/admin" exact Component={Dashboard} />
+        {/* <AdminTemplate path="/admin" exact Component={Dashboard} />
         <AdminTemplate path="/admin/films" exact Component={Films} />
         <AdminTemplate path="/admin/films/addnew" exact Component={AddNew} />
         <AdminTemplate path="/admin/films/edit/:id" exact Component={Edit} />
         <AdminTemplate path="/admin/films/showtime/:id/:tenphim" exact Component={ShowTime} />
 
-        <AdminTemplate path="/admin/users" exact Component={Dashboard} />
+        <AdminTemplate path="/admin/users" exact Component={Dashboard} /> */}
         {/* <AdminTemplate path="/admin/showtimes" exact Component={ShowTime} /> */}
         <HomeTemplate path="/" exact Component={Home} />
       
